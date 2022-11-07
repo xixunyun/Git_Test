10b3b0895a4e71fc1f788476c5fa40f40c7bf334
XiXun <2492405542@qq.com>
2022-11-07 15:00:31 +0800

    提交Test3

--- src/main/java/com/xixun/git/GitTest.java
+++ src/main/java/com/xixun/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git -夕曛");
         System.out.println("Hello Git2 -夕曛");
-        System.out.println("Hello Git2 -夕曛");
+        System.out.println("Hello Git3 -夕曛");
     }
 }
